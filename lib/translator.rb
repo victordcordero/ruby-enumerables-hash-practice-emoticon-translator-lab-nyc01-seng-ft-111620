300f706d69889e1762893537b0490ac8bdb4431a
Victor Cordero <victordcordero@gmail.com>
2020-11-12 23:34:00 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -26,7 +26,7 @@ def get_japanese_emoticon(path, emoticon)
   def get_english_meaning(path, emoticon)
     emoji = load_library(path)
     if emoji[:english].has_key?(emoticon)
-      emoji[:japanese][emoticon]
+      emoji[:english][emoticon]
     else
       return "Sorry, that emoticon was not found"
     end
