4324b6c7ab9359c3335d52cfb8e290d464b3db1e
Victor Cordero <victordcordero@gmail.com>
2020-11-12 21:58:56 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,18 +1,17 @@
 require "yaml"
 require "pry"
 
-def load_library(library)
-  new_hash = {
-   "get_meaning" => {},
-   "get_emoticon" => {}
- }
- library.each do |meaning, translation|
-   english = translation[0]
-   japanese = translation[1]
-   new_hash["get_meaning"][japanese] = meaning
-   new_hash["get_emoticon"][english] = japanese
- end
-  return new_hash
+def load_library(path)
+  emoji_hash = {}
+  emoji_hash["get_meaning"] = {}
+  emoji_hash["get_emoticon"] = {}
+  emoji = YAML.load_file(path)
+  emoji.each do |word, emojis|
+    emoji_hash["get_meaning"][emojis[1]] = word
+    emoji_hash["get_emoticon"][emojis[0]] = emojis[1]
+    binding.pry
+  end
+  emoji_hash
 end
 
 def get_japanese_emoticon(path, emoticon)
